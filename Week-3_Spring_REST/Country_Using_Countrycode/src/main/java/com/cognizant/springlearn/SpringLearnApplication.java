e8c757ee70d25ec5ecbd1a56a41eb34668d381f7
kaaviya23102090 <kaaviyasivasundar@gmail.com>
2026-07-11 13:05:32 +0530

    Country using code

--- Week-3_Spring_REST/Country_Using_Countrycode/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week-3_Spring_REST/Country_Using_Countrycode/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 @SpringBootApplication
 public class SpringLearnApplication {
--- Week-3_Spring_REST/Country_Using_Countrycode/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Week-3_Spring_REST/Country_Using_Countrycode/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,25 +1,25 @@
 package com.cognizant.springlearn.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+
 @RestController
-public class HelloController {
+public class CountryController {
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(HelloController.class);
+    @Autowired
+    private CountryService countryService;
 
-    @GetMapping("/hello")
-    public String sayHello() {
+    @GetMapping("/countries/{code}")
+    public Country getCountry(
+            @PathVariable String code) {
 
-        LOGGER.info("START");
+        return countryService.getCountry(code);
 
-        String message = "Hello World!!";
-
-        LOGGER.info("END");
-
-        return message;
     }
+
 }
