aeb76875d8f710b148cdebc17572c6f2109fd44b
kaaviya23102090 <kaaviyasivasundar@gmail.com>
2026-07-11 22:22:14 +0530

    Account Microservices

---
+++ Week-2-SpringCore_And_Maven/Week-4_Micro_Services/src/test/java/com/cognizant/week4_micro_services/Week4MicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.week4_micro_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4MicroServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
