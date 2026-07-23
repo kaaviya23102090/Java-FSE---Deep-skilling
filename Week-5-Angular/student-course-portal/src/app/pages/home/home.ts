bc7008585c62c08047594b01e94f4bcbf0b50518
kaaviya23102090 <kaaviyasivasundar@gmail.com>
2026-07-23 10:35:55 +0530

    Added comments

--- Week-5-Angular/student-course-portal/src/app/pages/home/home.ts
+++ Week-5-Angular/student-course-portal/src/app/pages/home/home.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  message = '';
+  searchTerm = '';
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+  portalName = 'Student Course Portal';
+  isPortalActive = true;
+}
